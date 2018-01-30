22ab611b5c784f4924dce2b29ee61d15e7e33c69
danlei <jne0915@gmail.com>
2018-01-30 15:40:41 +0800

    add rnn

--- prop_conv1d.py
+++ prop_conv1d.py
@@ -38,42 +38,55 @@ class DSet(Dataset):
 
 class CNN(torch.nn.Module):
 
-    def __init__(self, D_in):
+    def __init__(self, input_size):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv1d(1, 8, 3, padding=1)
         self.conv2 = nn.Conv1d(8, 16, 3, padding=1)
-        # self.conv3 = nn.Conv1d(16, 32)
-        # self.conv4 = nn.Conv1d(32, 64)
-        self.fc_in = D_in // 4 * 16
+        self.fc_in = input_size // 4 * 16
         self.fc1 = nn.Linear(self.fc_in, 64)
         self.fc2 = nn.Linear(64, 1)
 
     def forward(self, x):
         x = F.relu(F.max_pool1d(self.conv1(x), 2))
         x = F.relu(F.max_pool1d(self.conv2(x), 2))
-        x = F.dropout(x.view(-1, self.fc_in))
+        x = F.dropout(x.view(-1, self.fc_in), training=self.training)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.sigmoid(x)
 
 
-if __name__ == '__main__':
+class RNN(torch.nn.Module):
 
-    print('loading data ...')
-    samples = glob('rumor/*.json') + glob('truth/*.json')
-    train_data, test_data = train_test_split(samples, test_size=0.2, random_state=42)
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_gpu else {}
-    train_loader = DataLoader(DSet(train_data), batch_size=128, **kwargs)
-    test_loader = DataLoader(DSet(test_data), batch_size=128, **kwargs)
+    def __init__(self, input_size, hidden_size=64, bidirectional=True):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.n_directions = 2 if bidirectional else 1
+        self.rnn = nn.GRU(input_size, hidden_size, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * self.n_directions, 1)
+
+    def forward(self, x):
+        h0 = self._init_hidden_state(x.size(0))
+        x, hn = self.rnn(x, h0)
+        x = self.fc(x[:, -1, :])
+        return F.sigmoid(x)
+
+    def _init_hidden_state(self, batch_size):
+        h0 = torch.zeros(self.n_directions, batch_size, self.hidden_size)
+        if use_gpu:
+            h0 = h0.cuda()
+        return Variable(h0)
+
+
+def train_cnn(step=100, n_epoch=10):
 
     print('building model ...')
-    model = CNN(100)
+    model = CNN(step)
     if use_gpu:
         model.cuda()
     criterion = nn.BCELoss()
     optimizer = optim.RMSprop(model.parameters())
 
-    for epoch in range(10):
+    for epoch in range(n_epoch):
         print('Epoch %03d:' % (epoch + 1))
         train_loss, test_loss, tr_acc, val_acc = 0.0, 0.0, 0.0, 0.0
         model.train()
@@ -111,3 +124,62 @@ if __name__ == '__main__':
         val_acc = val_acc / len(test_loader.dataset) * 100
         print('val_loss %.6f | val_acc %.2f%%' % (test_loss, val_acc))
 
+
+def train_rnn(step_size=10, n_epoch=10):
+
+    print('building model ...')
+    model = RNN(step_size)
+    if use_gpu:
+        model.cuda()
+    criterion = nn.BCELoss()
+    optimizer = optim.RMSprop(model.parameters())
+
+    for epoch in range(n_epoch):
+        print('Epoch %03d:' % (epoch + 1))
+        train_loss, test_loss, tr_acc, val_acc = 0.0, 0.0, 0.0, 0.0
+        model.train()
+        for data, target in train_loader:
+            data = data.view(data.size(0), -1, step_size)
+            target = target.view(target.size(0), 1)
+            optimizer.zero_grad()
+            if use_gpu:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.data[0] * data.size(0)
+            pred = torch.sign(output.data - 0.5).clamp_(min=0)
+            tr_acc += pred.eq(target.data).cpu().sum()
+        train_loss /= len(train_loader.dataset)
+        tr_acc = tr_acc / len(train_loader.dataset) * 100
+        print('tr_loss %.6f | tr_acc %.2f%%' % (train_loss, tr_acc))
+
+        model.eval()
+        for data, target in test_loader:
+            data = data.view(data.size(0), -1, step_size)
+            target = target.view(target.size(0), 1)
+            if use_gpu:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data, volatile=True), Variable(target)
+            output = model(data)
+            loss = criterion(output, target)
+            test_loss += loss.data[0] * data.size(0)
+            pred = torch.sign(output.data - 0.5).clamp_(min=0)
+            val_acc += pred.eq(target.data).cpu().sum()
+        test_loss /= len(test_loader.dataset)
+        val_acc = val_acc / len(test_loader.dataset) * 100
+        print('val_loss %.6f | val_acc %.2f%%' % (test_loss, val_acc))
+
+
+if __name__ == '__main__':
+
+    print('loading data ...')
+    samples = glob('rumor/*.json') + glob('truth/*.json')
+    train_data, test_data = train_test_split(samples, test_size=0.2, random_state=42)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_gpu else {}
+    train_loader = DataLoader(DSet(train_data), batch_size=128, **kwargs)
+    test_loader = DataLoader(DSet(test_data), batch_size=128, **kwargs)
+
+    train_rnn(10, 10)
