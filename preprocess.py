d66a9f3669fde5bfa8c320920ca42c13933f0a70
danlei <jne0915@gmail.com>
2018-02-26 19:19:46 +0800

    cut content

--- preprocess.py
+++ preprocess.py
@@ -27,6 +27,24 @@ def get_prop_info():
     np.savez('data/prop_span.npz', **data)
 
 
+def get_content():
+
+    import thulac
+
+    thul = thulac.thulac(seg_only=True)
+    os.mkdir('data/original_content')
+    os.mkdir('data/cutted_content')
+    for item in (glob('rumor/*.json') + glob('truth/*.json')):
+        fid = os.path.basename(item)[:-5]
+        original = f'data/original_content/{fid}.txt'
+        cutted = f'data/cutted_content/{fid}.txt'
+        raw = json.load(open(item))
+        data = list(map(lambda x: x['text'] + '\n', raw))
+        with open(original, 'w') as f:
+            f.writelines(data)
+        thul.cut_f(original, cutted)
+
+
 if __name__ == '__main__':
 
-    get_prop_info()
+    get_content()
